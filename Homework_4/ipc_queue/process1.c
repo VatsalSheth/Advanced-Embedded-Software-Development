8782bccfeebdb759c996d5649c9537f76ec3bd9d
VatsalSheth <vash3665@colorado.edu>
2019-03-01 14:41:43 -0700

    homework4

--- Homework_4/ipc_queue/process1.c
+++ Homework_4/ipc_queue/process1.c
@@ -19,7 +19,7 @@
 #include <signal.h>
 
 #define queue_name "/ipc_queue"
-#define log_file "log_signal.txt"
+#define log_file "log.txt"
 
 #define handle_error(msg) \
 			{ perror(msg); \
@@ -32,13 +32,10 @@ struct data
 	int led;
 };
 
-struct timespec start = {0,0};
-struct timespec final = {0,0};
 struct timespec diff = {0,0};
 FILE *fp;
 mqd_t q_id;
 
-void time_diff(struct timespec *time2, struct timespec *time1, struct timespec *time_res);
 void set_signal_handler(void);
 
 int main()
@@ -48,22 +45,19 @@ int main()
 	char tmp[32];
 	int rc, i, rnd;
 	
-	clock_gettime(CLOCK_REALTIME, &start);
-	
 	remove(log_file);
 	set_signal_handler();
 	
 	srand(time(0));
 	
-	clock_gettime(CLOCK_REALTIME, &final);
-	time_diff(&final, &start, &diff);
-	
+	clock_gettime(CLOCK_REALTIME, &diff);
+		
 	fp = fopen(log_file, "a");
 	if(fp == NULL)
 	{
 		printf("Error in opening file %s from main thread\n",log_file);
 	}
-	fprintf(fp, "\n[%ld : %ld : %ld : %ld] Process 1 Main Thread IPC: POSIX Queue:\nPOSIX thread ID: %ld\nLinux thread ID %ld\n\n",diff.tv_sec, (diff.tv_nsec / 1000000), (diff.tv_nsec / 1000), diff.tv_nsec, pthread_self(), syscall(SYS_gettid));
+	fprintf(fp, "\n[%ld : %ld : %ld : %ld] Process 1 Main Thread IPC: POSIX Queue:\nProcess ID:%d\n\n",diff.tv_sec, (diff.tv_nsec / 1000000), (diff.tv_nsec / 1000), diff.tv_nsec, getpid());
 	
 	q_attr.mq_flags = 0;
     q_attr.mq_maxmsg = 10;
@@ -80,8 +74,7 @@ int main()
 		if(rc == -1)
 			handle_error("mq_receive");
 		
-		clock_gettime(CLOCK_REALTIME, &final);
-		time_diff(&final, &start, &diff);
+		clock_gettime(CLOCK_REALTIME, &diff);
 		fprintf(fp, "[%ld : %ld : %ld : %ld] Received: String = \"%s\"\tLength = %d\tLED = %d\n",diff.tv_sec, (diff.tv_nsec / 1000000), (diff.tv_nsec / 1000), diff.tv_nsec, receive[i].str, receive[i].string_length, receive[i].led);	
 		
 		rnd = rand();
@@ -90,8 +83,7 @@ int main()
 		send[i].string_length = strlen(send[i].str);
 		send[i].led = rnd%2;
 		
-		clock_gettime(CLOCK_REALTIME, &final);
-		time_diff(&final, &start, &diff);
+		clock_gettime(CLOCK_REALTIME, &diff);
 		
 		rc = mq_send(q_id, (const char *)&send[i], sizeof(struct data), 0); 
 		if(rc == -1)
@@ -111,32 +103,11 @@ int main()
 	return 0;
 }
 
-void time_diff(struct timespec *time2, struct timespec *time1, struct timespec *time_res)
-{
-  int dt_sec=time2->tv_sec - time1->tv_sec;
-  int dt_nsec=time2->tv_nsec - time1->tv_nsec;
-
-  if(dt_sec >= 0)
-  {
-    if(dt_nsec >= 0)
-    {
-      time_res->tv_sec=dt_sec;
-      time_res->tv_nsec=dt_nsec;
-    }
-    else
-    {
-      time_res->tv_sec=dt_sec-1;
-      time_res->tv_nsec=1000000000+dt_nsec;
-    }
-  }
-}
-
 void signal_handler(int signo, siginfo_t *info, void *extra) 
 {
 	int rc;
 	
-	clock_gettime(CLOCK_REALTIME, &final);
-	time_diff(&final, &start, &diff);
+	clock_gettime(CLOCK_REALTIME, &diff);
 	fprintf(fp, "[%ld : %ld : %ld : %ld] SIGINT encountered...!!!\n",diff.tv_sec, (diff.tv_nsec / 1000000), (diff.tv_nsec / 1000), diff.tv_nsec); 
 	
 	mq_close(q_id);
