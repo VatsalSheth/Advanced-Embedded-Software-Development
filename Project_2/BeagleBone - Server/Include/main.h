e1601db797cbd4ba38490ff68e9929be63afaec5
Vatsal Sheth <vash3665@colorado.edu>
2019-04-16 13:40:16 -0600

    Linux empty project created

---
+++ Project_2/BeagleBone - Server/Include/main.h
@@ -0,0 +1,14 @@
+/***********************************************************************************
+* @main.h
+* @This file contains dependent include files and variable declaration for main.c
+*
+* @author Vatsal Sheth & Sarthak Jain
+************************************************************************************/
+
+#include <unistd.h>
+#include <fcntl.h>           
+#include <sys/stat.h>        
+#include <string.h>
+#include <sys/syscall.h>
+#include <signal.h>
+#include <time.h>
---
+++ Project_2/BeagleBone - Server/Source/main.c
@@ -0,0 +1,16 @@
+/***********************************************************************************
+* @main.c
+* @This file contains main(), with thread creation, joining, heartbeat check function, 
+* GPIO interrupt thread. built in self test function.
+* Command to run the application project_1 <Log file name> <Log Verbosity>
+* @author Vatsal Sheth & Sarthak Jain
+************************************************************************************/
+
+#include "../Include/main.h"
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
+
